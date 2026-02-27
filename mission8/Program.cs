8d420c56aba84a74a315838e87e054bcf6c49f67
Alec Neish <alecneish@gmail.com>
2026-02-26 20:53:55 -0700

    Auto-apply migrations on startup to create Tasks table

--- mission8/Program.cs
+++ mission8/Program.cs
@@ -16,6 +16,12 @@ builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
